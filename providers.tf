61b8b25f8dc71fcc8a0bd126163b90fd5621412c
kleberson1983 <kleberson.brandao@hotmail.com>
2021-10-23 14:01:25 -0300

    Terraform GCP Basics

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.89.0"
+    }
+  }
+}
+
+# Configura o Provider Google Cloud com o Projeto
+provider "google" {
+
+  credentials = file("/home/vagrant/gcpterraformkey.json")
+
+  project = "labdevopscloudkleberson"
+  region  = "us-west1"
+  zone    = "us-west1-b"
+}
